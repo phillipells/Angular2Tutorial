0a14fa50129da33c334a5ace3ba206eef58c8653
phillipells <phillip.ells1025@gmail.com>
2017-03-23 18:15:23 -0500

    adding routing for individual heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,10 @@ import { DashboardComponent } from './dashboard.component';
       {
         path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
       }
     ])
   ],
