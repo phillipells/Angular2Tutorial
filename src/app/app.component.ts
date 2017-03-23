bb9fa8fe3de178853e1b033a999921a044ea633c
phillipells <phillip.ells1025@gmail.com>
2017-03-22 21:55:25 -0500

    updating to add hero-detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -13,10 +14,7 @@ const HEROES: Hero[] = [
   { id: 20, name: 'Tornado' }
 ];
 
-export class Hero {
-  id: number;
-  name: string;
-}
+
 
 @Component({
   selector: 'app-root',
